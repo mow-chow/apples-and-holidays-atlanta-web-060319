4d966c28f5f928aaef22799605e22a1a42fde668
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-23 12:21:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,8 +23,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_hash[:winter].map do |holiday, supplies|
-    supplies
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies << supply
   end.flatten
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
