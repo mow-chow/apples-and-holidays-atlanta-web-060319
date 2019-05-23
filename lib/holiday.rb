734fa29b69313dd3bb202f61f859901b88874e0b
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-23 12:09:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,9 +23,7 @@ holiday_hash[:spring][:memorial_day] << 'Fun'
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-season = "summer"
-holiday_name =  :fathers_day
-supply_array = ["Mug"]
+
 holiday_hash[:summer] << (holiday_name, supply_array)
   # code here
   # remember to return the updated hash
